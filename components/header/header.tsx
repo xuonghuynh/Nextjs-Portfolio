2e2b76809a7b4b67af04e987da272c9f15318795
Xuong Huynh <longxuongz@gmail.com>
2024-04-05 16:34:45 +0700

    added typewritter

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -4,8 +4,21 @@ import { FaMapMarkerAlt } from "react-icons/fa";
 import { BsTerminalFill } from "react-icons/bs";
 import SubInfo from "@/components/sub-info";
 import { IoIosSchool } from "react-icons/io";
+import { TypewriterEffectSmooth } from "@/components/ui/typewriter-effect";
 
 const Header = () => {
+    const words = [
+        {
+          text: "Huỳnh",
+        },
+        {
+          text: "Long",
+        },
+        {
+          text: "Xưỡng",
+          className: "text-blue-500 dark:text-blue-500",
+        },
+      ];
     return (
         <div className="mt-20">
             <div className="flex items-center gap-x-7 flex-col md:flex-row pb-7">
@@ -15,12 +28,12 @@ const Header = () => {
                 </Avatar>
                 <div className="flex-1">
                     <h1 className="font-bold text-5xl dark:text-white text-violet-700">
-                        Huỳnh Long Xưỡng
+                    <TypewriterEffectSmooth words={words} />
                     </h1>
                     <h2 className="mt-2 text-lg md:text-xl font-semibold md:font-bold capitalize dark:text-zinc-400">
                         Frontend Developer
                     </h2>
-                    <div className="mt-2 flex flex-wrap items-center gap-y-1 gap-x-4">
+                    <div className="mt-3 flex flex-wrap items-center gap-y-1 gap-x-4">
                         <SubInfo icon={FaMapMarkerAlt} label="Ho Chi Minh, Vietnam" />
                         <SubInfo icon={BsTerminalFill} label="5 Years of experience" />
                         <SubInfo icon={IoIosSchool} label="BSc FPT Greenwich" />
