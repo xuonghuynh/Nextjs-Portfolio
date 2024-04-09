19e5f6b75fbff09a587f9ce8d514cd3ba9fd2876
Xuong Huynh <longxuongz@gmail.com>
2024-04-09 14:53:25 +0700

    Update Header

--- app/(Home)/_components/Header.tsx
+++ app/(Home)/_components/Header.tsx
@@ -9,62 +9,48 @@ import { TypeAnimation } from "react-type-animation";
 import Particle from "@/components/ui/particles";
 
 const Header = () => {
-    const words = [
-        {
-            text: "Huỳnh",
-        },
-        {
-            text: "Long",
-        },
-        {
-            text: "Xưỡng",
-            className: "text-blue-500 dark:text-blue-500",
-        },
-    ];
-    return (
-        <div className="relative py-20 h-[60dvh]">
-            <Particle />
-            <div className="max-w-screen-xl mx-auto px-4 grid grid-rows-1 grid-rows-reverse md:grid-cols-2 w-full h-full items-center">
-                <div>
-                    <div className="font-bold dark:text-white text-violet-700">
-                        <h1 className="text-[35px] md:text-[50px] font-bold uppercase">HI, I&apos;M <span className="text-yellow-400 capitalize"> XƯỠNG</span></h1>
-                        <TypeAnimation
-                            sequence={[
-                                // Same substring at the start will only be typed out once, initially
-                                "Frontend Developer",
-                                1500, // wait 1s before replacing "Mice" with "Hamsters"
-                                "Programmer",
-                                1500,
-                                "Coder",
-                                1500,
-                            ]}
-                            wrapper="span"
-                            speed={50}
-                            style={{
-                                // fontSize: "45px",
-                                display: "inline-block",
-                                textTransform: "uppercase"
-                            }}
-                            repeat={Infinity}
-                            className="text-[35px] md:text-[3rem] text-[#55e6e5] font-bold"
-                        />
-                    </div>
-                    <div className="mt-3 flex flex-wrap items-center gap-y-1 gap-x-4">
-                        <SubInfo
-                            icon={FaMapMarkerAlt}
-                            label="Ho Chi Minh, Vietnam"
-                        />
-                        <SubInfo
-                            icon={BsTerminalFill}
-                            label="5 Years of experience"
-                        />
-                        <SubInfo icon={IoIosSchool} label="BSc FPT Greenwich" />
-                    </div>
-                </div>
-                <div className="bg-[url('/avatar-1.png')] w-[200px] h-[200px] md:w-[400px] md:h-[400px] animate-profile shadow-profile bg-cover bg-center bg-no-repeat mx-auto z-50" />
-            </div>
+  return (
+    <div className="relative h-[700px] py-20 lg:h-[60dvh]">
+      <Particle />
+      <div className="grid-reverse mx-auto grid h-full w-full max-w-screen-xl grid-rows-1 items-center px-4 lg:grid-cols-2">
+        <div className="order-2 lg:order-1">
+          <div className="text-center font-bold text-violet-700 dark:text-white xl:text-left">
+            <h1 className="text-[26px] font-bold uppercase md:text-[3rem]">
+              HI, I&apos;M{" "}
+              <span className="capitalize text-yellow-400"> XƯỠNG</span>
+            </h1>
+            <TypeAnimation
+              sequence={[
+                "Frontend Developer",
+                1500,
+                "Web Developer",
+                1500,
+                "Programmer",
+                1500,
+                "Coder",
+                1500,
+              ]}
+              wrapper="span"
+              speed={50}
+              style={{
+                // fontSize: "45px",
+                display: "inline-block",
+                textTransform: "uppercase",
+              }}
+              repeat={Infinity}
+              className="text-[26px] font-bold text-[#55e6e5] md:text-[3rem]"
+            />
+          </div>
+          <div className="mt-3 flex flex-col flex-wrap items-center justify-center gap-x-4 gap-y-2 md:flex-row md:items-start xl:justify-start">
+            <SubInfo icon={FaMapMarkerAlt} label="Ho Chi Minh, Vietnam" />
+            <SubInfo icon={BsTerminalFill} label="5 Years of experience" />
+            <SubInfo icon={IoIosSchool} label="BSc FPT Greenwich" />
+          </div>
         </div>
-    );
+        <div className="z-50 order-1 mx-auto h-[220px] w-[220px] animate-profile bg-[url('/avatar-1.png')] bg-cover bg-center bg-no-repeat shadow-profile md:h-[280px] md:w-[280px] lg:order-2 lg:h-[400px] lg:w-[400px]" />
+      </div>
+    </div>
+  );
 };
 
 export default Header;
