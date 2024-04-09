a07b1b8778cbc5574b9f4daf86b8b9028507c144
Xuong Huynh <longxuongz@gmail.com>
2024-04-09 16:05:41 +0700

    fixed render and add motion

--- app/(Home)/_components/WorkHistorySection.tsx
+++ app/(Home)/_components/WorkHistorySection.tsx
@@ -1,42 +1,70 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import {
     VerticalTimeline,
     VerticalTimelineElement,
 } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
+import {motion} from "framer-motion"
 
 import { data } from "@/data/data";
 import Title from "@/app/(Home)/_components/Title";
 import { useTheme } from "next-themes";
 
+const slideVariant = {
+    initial: {
+        opacity: 0,
+        x: -500
+    },
+    animate: {
+        opacity: 1,
+        x: 0,
+        transition: {
+            duration: 1,
+            staggerChildren: 0.1
+        }
+    }
+}
+
 const WorkHistory = () => {
+    const [isHide, setIsHide] = useState(true);
     const { theme } = useTheme();
     const { experients } = data;
+
+    setTimeout(() => setIsHide(false));
 
     return (
         <div className="max-w-screen-xl mx-auto px-4 w-full border-t border-zinc-100 py-7 dark:border-zinc-800">
             <Title title="Work History" />
-            <VerticalTimeline lineColor={theme === "dark" ? "white" : "#F4F4F5"}>
-                {experients.map((item, index) => (
-                    <React.Fragment key={index}>
-                        <VerticalTimelineElement
-                            key={item.id}
-                            date={item.time}
-                            icon={item.icon}
-                            iconStyle={{ background: theme === "dark" ? "#27272A" : "white", color: theme === "dark" ? "white" : "#6d28d9" }}
-                            visible={true}
-                            contentStyle={{ background: theme === 'dark' ? "#27272A" : "#F4F4F5", color: "white", boxShadow: "none" }}
-                            contentArrowStyle={{ borderRight: theme === 'dark' ? "0.4rem solid #27272A" : "0.4rem solid #F4F4F5" }}
-                        >
-                            <h3 className="text-zinc-700 dark:text-white capitalize text-sm md:text-base font-bold">
-                                {item.title}
-                            </h3>
-                            <div className="text-base md:text-lg font-semibold text-black dark:text-zinc-400">{item.description}</div>
-                        </VerticalTimelineElement>
-                    </React.Fragment>
-                ))}
-            </VerticalTimeline>
+            {!isHide && (
+                <motion.div
+                variants={slideVariant}
+                initial="initial"
+                whileInView={"animate"}
+                viewport={{ once: true }}
+            >
+                <VerticalTimeline lineColor={theme === "dark" ? "white" : "#F4F4F5"}>
+                    {experients.map((item, index) => (
+                        <React.Fragment key={index}>
+                            <VerticalTimelineElement
+                                key={item.id}
+                                date={item.time}
+                                icon={item.icon}
+                                iconStyle={{ background: theme === "dark" ? "#27272A" : "white", color: theme === "dark" ? "white" : "#6d28d9" }}
+                                visible={true}
+                                contentStyle={{ background: theme === 'dark' ? "#27272A" : "#F4F4F5", color: "white", boxShadow: "none" }}
+                                contentArrowStyle={{ borderRight: theme === 'dark' ? "0.4rem solid #27272A" : "0.4rem solid #F4F4F5" }}
+                            >
+                                <h3 className="text-zinc-700 dark:text-white capitalize text-sm md:text-base font-bold">
+                                    {item.title}
+                                </h3>
+                                <div className="text-base md:text-lg font-semibold text-black dark:text-zinc-400">{item.description}</div>
+                            </VerticalTimelineElement>
+                        </React.Fragment>
+                    ))}
+                </VerticalTimeline>
+            </motion.div>
+            )}
         </div>
     );
 };
--- components/ui/particles.tsx
+++ components/ui/particles.tsx
@@ -8,12 +8,6 @@ import { useTheme } from "next-themes";
 const Particle = () => {
     const { theme } = useTheme();
     const particlesInit = useCallback(async (engine: Engine) => {
-        console.log(engine);
-
-        // you can initialize the tsParticles instance (engine) here, adding custom shapes or presets
-        // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-        // starting from v2 you can add only the features you need reducing the bundle size
-        //await loadFull(engine);
         await loadSlim(engine);
     }, []);
 
